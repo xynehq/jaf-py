04f4390791c1aec163f07a1087f519b24678dfa8
Satvik Batra <satvik.batra@juspay.in>
2025-08-08 17:14:24 +0530

    feat: removed js/ts code

--- src/core/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createRunId = exports.createTraceId = void 0;
-const createTraceId = (id) => id;
-exports.createTraceId = createTraceId;
-const createRunId = (id) => id;
-exports.createRunId = createRunId;
