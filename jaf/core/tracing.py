a00b424b13ff10513d541f414676056e23bb08f2
Harshpreet Singh <harshpreet.singh@juspay.in>
2025-10-08 15:49:14 +0530

    chore: Bump version to 2.5.3 in README, __init__, and pyproject.toml

--- jaf/core/tracing.py
+++ jaf/core/tracing.py
@@ -443,7 +443,7 @@ class LangfuseTraceCollector:
             public_key=public_key,
             secret_key=secret_key,
             host=host,
-            release="jaf-py-v2.5.2",
+            release="jaf-py-v2.5.3",
             httpx_client=client
         )
         self._httpx_client = client
