c697f33471c72d51a40218d5a2c08924f5b58a13
Harshpreet Singh <122544630+harshpreet931@users.noreply.github.com>
2025-10-17 18:02:11 +0530

    chore: version bump (#52)
    * chore: bump version to 2.5.6 in README, __init__, and pyproject.toml

--- jaf/core/tracing.py
+++ jaf/core/tracing.py
@@ -443,7 +443,7 @@ class LangfuseTraceCollector:
             public_key=public_key,
             secret_key=secret_key,
             host=host,
-            release="jaf-py-v2.5.5",
+            release="jaf-py-v2.5.6",
             httpx_client=client
         )
         self._httpx_client = client
