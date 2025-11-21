d54cd7189792d3679bbedc02d1480c4266f9d9a8
harshpreet931 <harshpreet0402@gmail.com>
2025-11-21 20:02:17 +0530

    chore(release): bump version to 2.6.2

--- jaf/__init__.py
+++ jaf/__init__.py
@@ -201,7 +201,7 @@ def generate_run_id() -> RunId:
     return create_run_id(str(uuid.uuid4()))
 
 
-__version__ = "2.6.1"
+__version__ = "2.6.2"
 __all__ = [
     # Core types and functions
     "TraceId",
