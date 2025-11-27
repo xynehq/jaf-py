ead5912796fce147101238266e162c03f106f94e
Harshpreet Singh <122544630+harshpreet931@users.noreply.github.com>
2025-11-27 15:26:19 +0530

    Update release version to jaf-py-v2.6.3

--- jaf/core/tracing.py
+++ jaf/core/tracing.py
@@ -469,7 +469,7 @@ class LangfuseTraceCollector:
             public_key=public_key,
             secret_key=secret_key,
             host=host,
-            release="jaf-py-v2.6.2",
+            release="jaf-py-v2.6.3",
             httpx_client=client,
         )
         self._httpx_client = client
