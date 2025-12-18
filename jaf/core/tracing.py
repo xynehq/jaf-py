9e45b288a79a35c8467e4ec6636f53e4657ba0bc
harshpreet931 <harshpreet0402@gmail.com>
2025-12-18 15:55:56 +0530

    chore: version bump 2.6.5

--- jaf/core/tracing.py
+++ jaf/core/tracing.py
@@ -469,7 +469,7 @@ class LangfuseTraceCollector:
             public_key=public_key,
             secret_key=secret_key,
             host=host,
-            release="jaf-py-v2.6.4",
+            release="jaf-py-v2.6.5",
             httpx_client=client,
         )
         self._httpx_client = client
